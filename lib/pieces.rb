97079ed49497e1f8849ffcefe8f1808ba13d449f
nolanborz <nolanrborzoni@gmail.com>
2024-08-10 10:24:06 -0600

    Add unicode chess symbols

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,12 +1,24 @@
+WHITE_PIECES = {
+  king: "♔", queen: "♕", rook: "♖",
+  bishop: "♗", knight: "♘", pawn: "♙"
+}
+
+BLACK_PIECES = {
+  king: "♚", queen: "♛", rook: "♜",
+  bishop: "♝", knight: "♞", pawn: "♟"
+}
+
 class Pawn
+  SYMBOLS = { white: "♙", black: "♟"}
   attr_reader :position, :color
   def initialize(x, y, color)
     @position = [x, y]
     @color = color
+    @symbol = SYMBOLS[color]
   end
 
   def inspect
-    "#<#{self.class} position=#{@position}, color=#{@color}>"
+    "#<#{self.class} #{@symbol} position=#{@position}, color=#{@color}>"
   end
 
   def forward_one
@@ -33,8 +45,51 @@ class Pawn
   end
 end
 
+class Rook
+  SYMBOLS = { white: "♖", black: "♜" }
+  attr_reader :position, :color
+  def initialize(x, y, color)
+    @position = [x, y]
+    @color = color
+    @symbol = SYMBOLS[@color]
+  end
+
+  def inspect
+    "#<#{self.class} position=#{@position}, color=#{@color}>"
+  end
+
+  def move_vertical(new_pos)
+    @position = [@position[0], new_pos]
+  end
+
+  def move_horizontal(new_pos)
+    @position = [new_pos, @position[1]]
+  end
+
+  def valid_move?
+    x.between?(0, 7) && y.between?(0, 7)
+  end
+end
+
+class Knight
+  attr_reader :position, :color
+  def initialize(x, y, color)
+    @position = [x, y]
+    @color = color
+  end
+
+  
+end
+
 pwn = Pawn.new(4, 6, :black)
 pwn.forward_two
-pwn.forward_one
 puts pwn.inspect
+
+rk = Rook.new(0, 0, :white)
+rk.move_horizontal(4)
+puts rk.inspect
+
+
+
+
 
