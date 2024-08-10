043f529e924289c303465d0a36ce519af92488c5
nolanborz <nolanrborzoni@gmail.com>
2024-08-10 08:32:25 -0600

    Add pawn class and some basic movement methods

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,19 +1,40 @@
 class Pawn
-  attr_accessor :position, :color
-  def initialize(position, color)
-    @position = position
+  attr_reader :position, :color
+  def initialize(x, y, color)
+    @position = [x, y]
     @color = color
   end
 
-  def forward_one
+  def inspect
+    "#<#{self.class} position=#{@position}, color=#{@color}>"
+  end
 
+  def forward_one
+    new_y = @color == :white ? @position[1] + 1 : @position[1] - 1
+    @position = [@position[0], new_y] if valid_move?(@position[0], new_y)
   end
   
   def forward_two
-
+    return unless (@color == :white && @position[1] == 1) || (@color == :black && @position[1] == 6)
+    new_y = @color == :white ? @position[1] + 2 : @position[1] - 2
+    @position = [@position[0], new_y] if valid_move?(@position[0], new_y)
   end
 
-  def take_piece(left_or_right)
+  def take_piece(direction)
+    new_x = direction == :left ? @position[0] - 1 : @position[0] + 1
+    new_y = @color == :white ? @position[1] + 1 : @position[1] - 1
+    @position = [new_x, new_y] if valid_move?(new_x, new_y)
+  end
 
+  private
+  
+  def valid_move?(x, y)
+    x.between?(0, 7) && y.between?(0, 7)
   end
 end
+
+pwn = Pawn.new(4, 6, :black)
+pwn.forward_two
+pwn.forward_one
+puts pwn.inspect
+
