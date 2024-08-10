51f51facd5eea8052bd3d2136ebcc66c0ab12bdf
nolanborz <nolanrborzoni@gmail.com>
2024-08-10 07:35:23 -0600

    Set up rubocop, rspec, and initial code files

---
+++ lib/pieces.rb
@@ -0,0 +1,19 @@
+class Pawn
+  attr_accessor :position, :color
+  def initialize(position, color)
+    @position = position
+    @color = color
+  end
+
+  def forward_one
+
+  end
+  
+  def forward_two
+
+  end
+
+  def take_piece(left_or_right)
+
+  end
+end
