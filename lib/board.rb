1ba0c33a1908842d1e81a443f78136d9b78c937d
nolanborz <nolanrborzoni@gmail.com>
2024-08-11 20:57:27 -0600

    Add some more functionality and pseudocode to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,11 @@
 class Board
-  def initialize(value)
-    @value = value
+  require_relative 'king'
+  attr_reader :current_player, :columns_arr
+  def initialize
+    @current_player = nil
+    @columns_arr = [1, 2, 3, 4, 5, 6, 7, 8]
   end
 end
+
+roger = Board.new
+dog = King.new(1, 1, :black)
--- lib/pieces.rb
+++ lib/pieces.rb
@@ -8,42 +8,7 @@ BLACK_PIECES = {
   bishop: "♝", knight: "♞", pawn: "♟"
 }
 
-class Pawn
-  SYMBOLS = { white: "♙", black: "♟"}
-  attr_reader :position, :color
-  def initialize(x, y, color)
-    @position = [x, y]
-    @color = color
-    @symbol = SYMBOLS[color]
-  end
 
-  def inspect
-    "#<#{self.class} #{@symbol} position=#{@position}, color=#{@color}>"
-  end
-
-  def forward_one
-    new_y = @color == :white ? @position[1] + 1 : @position[1] - 1
-    @position = [@position[0], new_y] if valid_move?(@position[0], new_y)
-  end
-  
-  def forward_two
-    return unless (@color == :white && @position[1] == 1) || (@color == :black && @position[1] == 6)
-    new_y = @color == :white ? @position[1] + 2 : @position[1] - 2
-    @position = [@position[0], new_y] if valid_move?(@position[0], new_y)
-  end
-
-  def take_piece(direction)
-    new_x = direction == :left ? @position[0] - 1 : @position[0] + 1
-    new_y = @color == :white ? @position[1] + 1 : @position[1] - 1
-    @position = [new_x, new_y] if valid_move?(new_x, new_y)
-  end
-
-  private
-  
-  def valid_move?(x, y)
-    x.between?(0, 7) && y.between?(0, 7)
-  end
-end
 
 class Rook
   SYMBOLS = { white: "♖", black: "♜" }
@@ -68,42 +33,6 @@ class Rook
 
   def valid_move?
     x.between?(0, 7) && y.between?(0, 7)
-  end
-end
-
-class Knight
-  SYMBOLS = { white: "♘", black: "♞" }
-  def initialize(x, y, color)
-    @position = [x, y]
-    @color = color
-    @symbol = SYMBOLS[@color]
-  end
-end
-
-class Bishop
-  SYMBOLS = { white: "♗", black: "♝" }
-  def initialize(x, y, color)
-    @position = [x, y]
-    @color = color
-    @symbol = SYMBOLS[@color]
-  end
-end
-
-class Queen 
-  SYMBOLS = { white: "♕" , black: "♛" }
-  def initialize(x, y, color)
-    @position = [x, y]
-    @color = color
-    @symbol = SYMBOLS[@color]
-  end
-end
-
-class King
-  SYMBOLS = { white: "♔", black: "♚" }
-  def initialize(x, y, color)
-    @position = [x, y]
-    @color = color
-    @symbol = SYMBOLS[@color]
   end
 end
 
